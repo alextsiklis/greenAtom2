e7c1156f47451215e3203cc5b31049e57b228f65
Alexander Tsiklis <footballtsiklis13@mail.ru>
2022-01-06 18:00:04 +0500

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-import entity.BookFlight;
-import entity.Booking_data;
-import entity.Customer;
 import entity.book_flight;
 import functions.PackageCreator;
 import functions.Transformer;
@@ -14,18 +11,10 @@ public class Main {
         PackageCreator.createPackage("ResultedXML");
 
         ArrayList<book_flight> book_flights = new ArrayList<>();
-
         book_flights.add(XmlReader.xmlReader("file"));
-
-//        Booking_data booking_data = new Booking_data("Аэрофлот", "SU1010", "business", "2022-01-01");
-//        Customer customer = new Customer("Иван", "Петров", booking_data);
-//        book_flights.add(new book_flight(customer));
 
         for(book_flight book_fli: book_flights) {
             XmlWriter.xmlWriter(Transformer.transform(book_fli));
         }
-
-//        BookFlight bookFlight = new BookFlight("01.01.2022", "SU1010", "Иван Петров", "business");
-//        XmlWriter.xmlWriter(bookFlight);
     }
 }
